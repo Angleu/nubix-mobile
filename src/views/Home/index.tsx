75cc61c98275d5f1c987d06a62c01d045eecf02c
Rafael Willen <rafaelpadre20@gmail.com>
2022-12-13 23:46:37 +0100

    Add home view header

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,19 +1,24 @@
-import { Avatar, HStack, VStack } from 'native-base';
+import { Avatar, Heading, HStack, Text, VStack } from 'native-base';
+import { BellSimple } from 'phosphor-react-native';
 import React from 'react';
-import { Text } from 'react-native';
 
 import Container from '../../components/layout/Container';
 
 export default function Home() {
   return (
     <Container>
-      <HStack py={12} justifyContent="space-between" alignItems="center">
-        <VStack>
-          <Text>Welcome back!</Text>
-          <Text>Hi, Zanuar Renaldie</Text>
+      <HStack pt={5} justifyContent="space-between" alignItems="center">
+        <VStack space={2}>
+          <Heading fontSize="xl" color="neutral.normal" fontWeight="bold">
+            Welcome back!
+          </Heading>
+          <Text color="black">Hi, Zanuar Renaldie</Text>
         </VStack>
-        <HStack>
-          <Avatar />
+        <HStack alignItems="center" space={5}>
+          <Avatar
+            source={{ uri: 'https://randomuser.me/api/portraits/women/45.jpg' }}
+          />
+          <BellSimple size={38} color="#3A3A3A" />
         </HStack>
       </HStack>
     </Container>
