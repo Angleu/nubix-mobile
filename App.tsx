733c97e8ec9755cea0eeadd99d76118fbc7421b8
Rafael Willen <rafaelpadre20@gmail.com>
2022-12-15 13:31:54 +0100

    Add safe area view and remove default header

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { extendTheme, NativeBaseProvider } from 'native-base';
 import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { RoutesEnum, Views } from './src/constants/routes';
 import { colorPallet } from './src/utils/theme';
@@ -12,18 +13,23 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NativeBaseProvider theme={theme}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName={RoutesEnum.Home}>
-          <Stack.Screen
-            name={RoutesEnum.Home}
-            component={Views[RoutesEnum.Home]}
-          />
-          <Stack.Screen
-            name={RoutesEnum.Transfer}
-            component={Views[RoutesEnum.Transfer]}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName={RoutesEnum.Home}
+            screenOptions={{ headerShown: false }}
+          >
+            <Stack.Screen
+              name={RoutesEnum.Home}
+              component={Views[RoutesEnum.Home]}
+            />
+            <Stack.Screen
+              name={RoutesEnum.Transfer}
+              component={Views[RoutesEnum.Transfer]}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
     </NativeBaseProvider>
   );
 }
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -17,6 +17,7 @@ import {
   Tray,
 } from 'phosphor-react-native';
 import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import Container from '../../components/layout/Container';
 
@@ -49,99 +50,110 @@ const activities = [
 
 export default function Home() {
   return (
-    <Container>
-      <HStack pt={5} justifyContent="space-between" alignItems="center">
-        <VStack space={2}>
-          <Heading fontSize="xl" color="neutral.normal" fontWeight="bold">
-            Welcome back!
-          </Heading>
-          <Text color="black">Hi, Zanuar Renaldie</Text>
-        </VStack>
-        <HStack alignItems="center" space={5}>
-          <Avatar
-            source={{ uri: 'https://randomuser.me/api/portraits/women/45.jpg' }}
-          />
-          <BellSimple size={38} color="#3A3A3A" />
-        </HStack>
-      </HStack>
-
-      <Box mt={10} bg="primary.lighter" px="8" py="4" rounded="3xl" shadow="9">
-        <HStack justifyContent="space-between" mb={8}>
-          <VStack>
-            <Text color="white" fontSize="md">
-              Total Balance
-            </Text>
-            <Text color="white" fontWeight="bold" fontSize="lg">
-              $ 188,290.90
-            </Text>
+    <SafeAreaView>
+      <Container>
+        <HStack pt={5} justifyContent="space-between" alignItems="center">
+          <VStack space={2}>
+            <Heading fontSize="xl" color="neutral.normal" fontWeight="bold">
+              Welcome back!
+            </Heading>
+            <Text color="black">Hi, Zanuar Renaldie</Text>
           </VStack>
-          <CreditCard color="#fff" size={35} />
-        </HStack>
-        <HStack justifyContent="flex-end">
-          <ShareNetwork size={26} color="white" />
-        </HStack>
-      </Box>
-
-      <HStack mt="12" justifyContent="space-between">
-        <VStack alignItems="center">
-          <Swap size={28} color="#3F5857" />
-          <Text>Transfer</Text>
-        </VStack>
-        <VStack alignItems="center">
-          <Tray size={28} color="#c4c4c4" />
-          <Text color="#c4c4c4">Deposit</Text>
-        </VStack>
-        <VStack alignItems="center">
-          <CreditCard size={28} color="#c4c4c4" />
-          <Text color="#c4c4c4">Pay</Text>
-        </VStack>
-        <VStack alignItems="center">
-          <Coins size={28} color="#c4c4c4" />
-          <Text color="#c4c4c4">Recieve</Text>
-        </VStack>
-      </HStack>
-
-      <VStack mt={6}>
-        <HStack justifyContent="space-between">
-          <Text color="#373737" fontWeight="semibold">
-            Recent Activity
-          </Text>
-          <Text color="#3F5857" fontWeight="bold">
-            See All
-          </Text>
+          <HStack alignItems="center" space={5}>
+            <Avatar
+              source={{
+                uri: 'https://randomuser.me/api/portraits/women/45.jpg',
+              }}
+            />
+            <BellSimple size={38} color="#3A3A3A" />
+          </HStack>
         </HStack>
 
-        <FlatList
-          data={activities}
-          mt={3}
-          renderItem={({ item }) => (
-            <HStack justifyContent="space-between" mb={4} alignItems="center">
-              <HStack space={3} alignItems="center">
-                {item.profilePic ? (
-                  <Avatar
-                    source={{ uri: item.profilePic }}
-                    bg="white"
-                    p={1}
-                    mx={1}
-                    shadow={4}
-                  />
-                ) : (
-                  <Box p={1} shadow={4} mx={2}>
-                    <PaperPlaneTilt color="#3F5857" size={32} />
-                  </Box>
-                )}
-                <VStack>
-                  <Text fontWeight="bold" color="#373737">
-                    {item.name}
-                  </Text>
-                  <Text color="#373737">{item.date}</Text>
-                </VStack>
+        <Box
+          mt={10}
+          bg="primary.lighter"
+          px="8"
+          py="4"
+          rounded="3xl"
+          shadow="9"
+        >
+          <HStack justifyContent="space-between" mb={8}>
+            <VStack>
+              <Text color="white" fontSize="md">
+                Total Balance
+              </Text>
+              <Text color="white" fontWeight="bold" fontSize="lg">
+                $ 188,290.90
+              </Text>
+            </VStack>
+            <CreditCard color="#fff" size={35} />
+          </HStack>
+          <HStack justifyContent="flex-end">
+            <ShareNetwork size={26} color="white" />
+          </HStack>
+        </Box>
+
+        <HStack mt="12" justifyContent="space-between">
+          <VStack alignItems="center">
+            <Swap size={28} color="#3F5857" />
+            <Text>Transfer</Text>
+          </VStack>
+          <VStack alignItems="center">
+            <Tray size={28} color="#c4c4c4" />
+            <Text color="#c4c4c4">Deposit</Text>
+          </VStack>
+          <VStack alignItems="center">
+            <CreditCard size={28} color="#c4c4c4" />
+            <Text color="#c4c4c4">Pay</Text>
+          </VStack>
+          <VStack alignItems="center">
+            <Coins size={28} color="#c4c4c4" />
+            <Text color="#c4c4c4">Recieve</Text>
+          </VStack>
+        </HStack>
+
+        <VStack mt={6}>
+          <HStack justifyContent="space-between">
+            <Text color="#373737" fontWeight="semibold">
+              Recent Activity
+            </Text>
+            <Text color="#3F5857" fontWeight="bold">
+              See All
+            </Text>
+          </HStack>
+
+          <FlatList
+            data={activities}
+            mt={3}
+            renderItem={({ item }) => (
+              <HStack justifyContent="space-between" mb={4} alignItems="center">
+                <HStack space={3} alignItems="center">
+                  {item.profilePic ? (
+                    <Avatar
+                      source={{ uri: item.profilePic }}
+                      bg="white"
+                      p={1}
+                      mx={1}
+                      shadow={4}
+                    />
+                  ) : (
+                    <Box p={1} shadow={4} mx={2}>
+                      <PaperPlaneTilt color="#3F5857" size={32} />
+                    </Box>
+                  )}
+                  <VStack>
+                    <Text fontWeight="bold" color="#373737">
+                      {item.name}
+                    </Text>
+                    <Text color="#373737">{item.date}</Text>
+                  </VStack>
+                </HStack>
+                <Text fontWeight="bold">{item.ammount}</Text>
               </HStack>
-              <Text fontWeight="bold">{item.ammount}</Text>
-            </HStack>
-          )}
-        />
-      </VStack>
-    </Container>
+            )}
+          />
+        </VStack>
+      </Container>
+    </SafeAreaView>
   );
 }
