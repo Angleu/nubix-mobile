5d1abff24e593de9d066a9c79bf8f0c869b86174
Rafael Willen <rafaelpadre20@gmail.com>
2022-12-26 18:37:55 +0100

    Add lottie react native

--- src/views/Payment/NFCPayment.tsx
+++ src/views/Payment/NFCPayment.tsx
@@ -1,11 +1,18 @@
 import { Entypo, MaterialCommunityIcons } from '@expo/vector-icons';
+import LottieView from 'lottie-react-native';
 import { Heading, Icon, IconButton, Text, VStack } from 'native-base';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import Container from '../../components/layout/Container';
 import Header from '../../components/layout/Header';
 
 const NFCPayment = () => {
+  const animation = useRef<LottieView>(null);
+
+  useEffect(() => {
+    animation.current.play(0, 138);
+  }, []);
+
   return (
     <Container>
       <Header
@@ -42,7 +49,17 @@ const NFCPayment = () => {
         />
       </VStack>
 
-      {/* Place animation here */}
+      <LottieView
+        autoPlay
+        loop
+        ref={animation}
+        style={{
+          width: 200,
+          height: 200,
+          backgroundColor: '#fff',
+        }}
+        source={require('../../../assets/animation/contactless-payment.json')}
+      />
 
       <Text
         textAlign="center"
