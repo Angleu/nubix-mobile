39e6e9380bf8fb8b796d7938a4321aab7fc56035
Rafael Willen <52548623+rafaelwillen@users.noreply.github.com>
2022-12-30 02:06:41 +0100

    Login Screen (#19)
    * Remove default app bar
    
    * Add the app logo
    
    * Add markup for the screen
    
    * Style the login screen
    
    * Add react hook form and zod
    
    * Add form controls
    
    * Add scrollable view
    
    * Add navigation to sign up screen

--- src/utils/theme/style.ts
+++ src/utils/theme/style.ts
@@ -1,7 +1,15 @@
-import { StyleProp, TextStyle } from 'react-native';
+import {
+  PressableAndroidRippleConfig,
+  StyleProp,
+  TextStyle,
+} from 'react-native';
 
 export const textShadowStyle: StyleProp<TextStyle> = {
   textShadowColor: 'rgba(0, 0, 0, 0.25)',
   textShadowOffset: { width: 0, height: 4 },
   textShadowRadius: 10,
+};
+
+export const androidRippleEffect: PressableAndroidRippleConfig = {
+  color: '#c4c4c4c4',
 };
--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,11 +1,186 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useNavigation } from '@react-navigation/native';
+import {
+  Button,
+  Center,
+  FormControl,
+  Heading,
+  HStack,
+  Icon,
+  Image,
+  Input,
+  ScrollView,
+  Switch,
+  Text,
+  VStack,
+} from 'native-base';
+import React, { useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+
+import LogoImage from '../../assets/images/logo.png';
+import Container from '../components/layout/Container';
+import { AuthStackNavigationProps } from '../routes/types';
+import { androidRippleEffect } from '../utils/theme/style';
+import loginSchema, { LoginFormType } from '../utils/validation/loginSchema';
 
 const Login = () => {
+  const [rememberUser, setRememberUser] = useState();
+
+  const { replace } = useNavigation<AuthStackNavigationProps<'Login'>>();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormType>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  const onSubmit = (data: LoginFormType) => {
+    console.log(data);
+  };
+
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
+    <Container>
+      <ScrollView>
+        <Center>
+          <Image source={LogoImage} alt="Tique Bank Logo" />
+          <Heading
+            my="25"
+            fontFamily="heading"
+            fontSize="md"
+            textTransform="uppercase"
+          >
+            Aceder a conta
+          </Heading>
+        </Center>
+
+        <Controller
+          name="email"
+          control={control}
+          render={({ field: { onChange, value } }) => (
+            <FormControl isRequired isInvalid={!!errors.email} mb="4">
+              <VStack>
+                <FormControl.Label>
+                  <Text fontSize="sm" color="light.500">
+                    EMAIL
+                  </Text>
+                </FormControl.Label>
+                <Input
+                  keyboardType="email-address"
+                  value={value}
+                  onChangeText={onChange}
+                  placeholder="example@mail.com"
+                  borderColor="primary.100"
+                  _focus={{
+                    bg: 'light.50',
+                    borderColor: 'primary.100',
+                  }}
+                  leftElement={
+                    <Icon
+                      name="email"
+                      as={MaterialIcons}
+                      color="primary.100"
+                      ml="3"
+                      size="lg"
+                    />
+                  }
+                />
+                <FormControl.ErrorMessage>
+                  {errors.email && errors.email.message}
+                </FormControl.ErrorMessage>
+              </VStack>
+            </FormControl>
+          )}
+        />
+
+        <Controller
+          name="password"
+          control={control}
+          render={({ field: { onChange, value } }) => (
+            <FormControl isRequired isInvalid={!!errors.password} mb="4">
+              <VStack>
+                <FormControl.Label>
+                  <Text fontSize="sm" color="light.500">
+                    PASSWORD
+                  </Text>
+                </FormControl.Label>
+                <Input
+                  value={value}
+                  onChangeText={onChange}
+                  secureTextEntry
+                  borderColor="primary.100"
+                  _focus={{
+                    bg: 'light.50',
+                    borderColor: 'primary.100',
+                  }}
+                  leftElement={
+                    <Icon
+                      name="lock"
+                      as={MaterialIcons}
+                      color="primary.100"
+                      ml="3"
+                      size="lg"
+                    />
+                  }
+                />
+                <FormControl.ErrorMessage>
+                  {errors.password && errors.password.message}
+                </FormControl.ErrorMessage>
+              </VStack>
+            </FormControl>
+          )}
+        />
+
+        <HStack alignItems="center" mb="6" space="2">
+          <Switch
+            value={rememberUser}
+            onToggle={(value) => setRememberUser(value)}
+            size="sm"
+            onTrackColor="primary.50"
+            onThumbColor="primary.100"
+          />
+          <Text opacity={0.7} fontSize="sm">
+            Iniciar sessão automaticamente
+          </Text>
+        </HStack>
+
+        <VStack space="5">
+          <Button
+            py="3"
+            bg="primary.100"
+            borderRadius="lg"
+            android_ripple={androidRippleEffect}
+            shadow="3"
+            _pressed={{
+              bg: 'primary.100',
+            }}
+            onPress={handleSubmit(onSubmit)}
+          >
+            Entrar
+          </Button>
+          <Button
+            variant="outline"
+            py="3"
+            color="primary.100"
+            borderRadius="lg"
+            _text={{
+              color: 'primary.100',
+            }}
+            borderColor="primary.100"
+            android_ripple={androidRippleEffect}
+            _pressed={{
+              bg: 'light.50',
+            }}
+            onPress={() => replace('SignUp')}
+          >
+            Criar Conta
+          </Button>
+        </VStack>
+
+        {/* TODO: Place other login methods here */}
+      </ScrollView>
+    </Container>
   );
 };
 
