ed4abe55747863c6f26b875eb6e8ef22aba6c1d2
Rafael Willen <rafaelpadre20@gmail.com>
2022-12-30 01:01:26 +0100

    Style the login screen

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,9 +1,11 @@
+import { MaterialIcons } from '@expo/vector-icons';
 import {
   Button,
   Center,
   FormControl,
   Heading,
   HStack,
+  Icon,
   Image,
   Input,
   Switch,
@@ -14,6 +16,7 @@ import React from 'react';
 
 import LogoImage from '../../assets/images/logo.png';
 import Container from '../components/layout/Container';
+import { androidRippleEffect } from '../utils/theme/style';
 
 // TODO: Add react hook form here
 const Login = () => {
@@ -21,33 +24,113 @@ const Login = () => {
     <Container>
       <Center>
         <Image source={LogoImage} alt="Tique Bank Logo" />
+        <Heading
+          my="25"
+          fontFamily="heading"
+          fontSize="md"
+          textTransform="uppercase"
+        >
+          Aceder a conta
+        </Heading>
       </Center>
 
-      <Heading>Aceder a conta</Heading>
-
-      <FormControl>
+      <FormControl isRequired mb="4">
         <VStack>
-          <FormControl.Label>EMAIL</FormControl.Label>
-          <Input placeholder="example@mail.com" />
+          <FormControl.Label>
+            <Text fontSize="sm" color="light.500">
+              EMAIL
+            </Text>
+          </FormControl.Label>
+          <Input
+            placeholder="example@mail.com"
+            borderColor="primary.100"
+            _focus={{
+              bg: 'light.50',
+              borderColor: 'primary.100',
+            }}
+            leftElement={
+              <Icon
+                name="email"
+                as={MaterialIcons}
+                color="primary.100"
+                ml="3"
+                size="lg"
+              />
+            }
+          />
           <FormControl.ErrorMessage>Email inválido</FormControl.ErrorMessage>
         </VStack>
       </FormControl>
-      <FormControl>
+      <FormControl isRequired mb="4">
         <VStack>
-          <FormControl.Label>PASSWORD</FormControl.Label>
-          <Input secureTextEntry />
+          <FormControl.Label>
+            <Text fontSize="sm" color="light.500">
+              PASSWORD
+            </Text>
+          </FormControl.Label>
+          <Input
+            secureTextEntry
+            borderColor="primary.100"
+            _focus={{
+              bg: 'light.50',
+              borderColor: 'primary.100',
+            }}
+            leftElement={
+              <Icon
+                name="lock"
+                as={MaterialIcons}
+                color="primary.100"
+                ml="3"
+                size="lg"
+              />
+            }
+          />
           <FormControl.ErrorMessage>Password inválida</FormControl.ErrorMessage>
         </VStack>
       </FormControl>
 
-      <HStack>
-        <Switch />
-        <Text>Iniciar sessão automaticamente</Text>
+      <HStack alignItems="center" mb="6" space="2">
+        <Switch
+          isChecked
+          size="sm"
+          onTrackColor="primary.50"
+          onThumbColor="primary.100"
+        />
+        <Text opacity={0.7} fontSize="sm">
+          Iniciar sessão automaticamente
+        </Text>
       </HStack>
 
-      <VStack>
-        <Button>Entrar</Button>
-        <Button>Criar Conta</Button>
+      <VStack space="5">
+        <Button
+          py="3"
+          bg="primary.100"
+          borderRadius="lg"
+          android_ripple={androidRippleEffect}
+          shadow="3"
+          _pressed={{
+            bg: 'primary.100',
+          }}
+        >
+          Entrar
+        </Button>
+        <Button
+          variant="outline"
+          py="3"
+          color="primary.100"
+          borderRadius="lg"
+          _text={{
+            color: 'primary.100',
+          }}
+          borderColor="primary.100"
+          android_ripple={androidRippleEffect}
+          shadow="3"
+          _pressed={{
+            bg: 'light.50',
+          }}
+        >
+          Criar Conta
+        </Button>
       </VStack>
 
       {/* TODO: Place other login methods here */}
