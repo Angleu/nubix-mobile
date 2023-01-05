dbce51e2c57e7a1941bd0d8fbcc51d1e4316a32a
Rafael Willen <rafaelpadre20@gmail.com>
2023-01-05 12:01:46 +0100

    Set the user authenticated

--- src/routes/RootRoute.tsx
+++ src/routes/RootRoute.tsx
@@ -2,7 +2,7 @@ import AuthRoute from './AuthRoute';
 import MainRoute from './MainRoute';
 
 const RootRoute = () => {
-  const isAuthenticated = false;
+  const isAuthenticated = true;
 
   if (isAuthenticated) return <MainRoute />;
   return <AuthRoute />;
