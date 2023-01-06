f3dc1c71639e76702a9d7fae51b02c0a05d4f6bb
Rafael Willen <rafaelpadre20@gmail.com>
2023-01-06 09:58:40 +0100

    Add the address api group

---
+++ src/api/address/types.ts
@@ -0,0 +1,15 @@
+// TODO: Add the type after the API is fixed
+export type GetAddressResponseType = unknown;
+
+export type CreateAddressRequestType = {
+  city: string;
+  country: string;
+  street: string;
+  houseNumber: string;
+  latitude: string;
+  longitude: string;
+  NIF: string;
+};
+
+// TODO: Add the type after the API is fixed
+export type CreateAddressResponseType = unknown;
