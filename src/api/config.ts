4ee127aff5dca93c23700016e663fd20179643db
Rafael Willen <rafaelpadre20@gmail.com>
2023-01-17 13:45:51 +0100

    HOTFIX: Update the API endpoint

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,7 @@
 import _axios from 'axios';
 
 const axios = _axios.create({
-  baseURL: 'https://bank-back-production.up.railway.app/',
+  baseURL: 'https://bank-back-production.up.railway.app/api',
 });
 
 export default axios;
