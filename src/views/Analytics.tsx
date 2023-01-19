3d829434b1175316b91cdbcd98002cf778e9a607
Rafael Willen <52548623+rafaelwillen@users.noreply.github.com>
2023-01-19 11:02:32 +0100

    Add scroll (#56)

--- src/views/Analytics.tsx
+++ src/views/Analytics.tsx
@@ -6,6 +6,7 @@ import {
   Icon,
   IconButton,
   Pressable,
+  ScrollView,
   Select,
   Text,
   theme,
@@ -88,185 +89,197 @@ const Analytics = () => {
         }
       />
 
-      <HStack justifyContent="flex-end" mt="8">
-        <Select
-          selectedValue={selectedYear}
-          flex={1}
-          _text={{
-            fontFamily: 'body',
-            fontSize: 'xs',
+      <ScrollView flex={1} showsVerticalScrollIndicator={false}>
+        <HStack justifyContent="flex-end" mt="8">
+          <Select
+            selectedValue={selectedYear}
+            flex={1}
+            _text={{
+              fontFamily: 'body',
+              fontSize: 'xs',
+            }}
+            _ios={{
+              py: '4',
+            }}
+            borderRadius="xl"
+            color="white"
+            accessibilityLabel="Escolhe o ano"
+            placeholder="Escolhe o ano"
+            onValueChange={setSelectedYear}
+            bg="primary.100"
+            placeholderTextColor="white"
+            defaultValue={listOfYears[listOfYears.length - 1].value}
+            dropdownIcon={
+              <Icon
+                as={Entypo}
+                name="chevron-down"
+                mr="4"
+                size="xl"
+                color="white"
+              />
+            }
+          >
+            {listOfYears.map(({ label, value }) => (
+              <Select.Item key={value} label={label} value={value} />
+            ))}
+          </Select>
+        </HStack>
+        <HStack my="2">
+          <Button
+            bg={isFirstHalf ? 'primary.100' : 'white'}
+            _text={{
+              color: isFirstHalf ? 'white' : 'primary.100',
+              fontFamily: 'body',
+            }}
+            borderWidth={isFirstHalf ? 0 : 1}
+            borderColor="primary.100"
+            borderRadius={0}
+            borderLeftRadius="lg"
+            flex={1}
+            variant="unstyled"
+            onPress={() => setFirstHalf(true)}
+          >
+            Janeiro - Junho
+          </Button>
+          <Button
+            bg={!isFirstHalf ? 'primary.100' : 'white'}
+            _text={{
+              color: !isFirstHalf ? 'white' : 'primary.100',
+              fontFamily: 'body',
+            }}
+            borderWidth={!isFirstHalf ? 0 : 1}
+            borderColor="primary.100"
+            borderRadius={0}
+            borderRightRadius="lg"
+            flex={1}
+            variant="unstyled"
+            onPress={() => setFirstHalf(false)}
+          >
+            Julho - Dezembro
+          </Button>
+        </HStack>
+
+        <View
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#fff',
           }}
-          _ios={{
-            py: '4',
-          }}
-          borderRadius="xl"
-          color="white"
-          accessibilityLabel="Escolhe o ano"
-          placeholder="Escolhe o ano"
-          onValueChange={setSelectedYear}
-          bg="primary.100"
-          placeholderTextColor="white"
-          defaultValue={listOfYears[listOfYears.length - 1].value}
-          dropdownIcon={
-            <Icon
-              as={Entypo}
-              name="chevron-down"
-              mr="4"
-              size="xl"
-              color="white"
+        >
+          <VictoryChart
+            width={Dimensions.get('screen').width}
+            theme={VictoryTheme.material}
+            animate
+          >
+            <VictoryAxis
+              animate
+              style={{
+                axis: { stroke: 'transparent' },
+                ticks: { stroke: 'transparent' },
+                tickLabels: {
+                  fill: theme.colors.light[800],
+                  fontSize: 16,
+                  fontWeight: 'bold',
+                },
+                grid: {
+                  stroke: 'transparent',
+                },
+              }}
             />
-          }
-        >
-          {listOfYears.map(({ label, value }) => (
-            <Select.Item key={value} label={label} value={value} />
-          ))}
-        </Select>
-      </HStack>
-      <HStack my="2">
-        <Button
-          bg={isFirstHalf ? 'primary.100' : 'white'}
-          _text={{
-            color: isFirstHalf ? 'white' : 'primary.100',
-            fontFamily: 'body',
-          }}
-          borderWidth={isFirstHalf ? 0 : 1}
-          borderColor="primary.100"
-          borderRadius={0}
-          borderLeftRadius="lg"
-          flex={1}
-          variant="unstyled"
-          onPress={() => setFirstHalf(true)}
-        >
-          Janeiro - Junho
-        </Button>
-        <Button
-          bg={!isFirstHalf ? 'primary.100' : 'white'}
-          _text={{
-            color: !isFirstHalf ? 'white' : 'primary.100',
-            fontFamily: 'body',
-          }}
-          borderWidth={!isFirstHalf ? 0 : 1}
-          borderColor="primary.100"
-          borderRadius={0}
-          borderRightRadius="lg"
-          flex={1}
-          variant="unstyled"
-          onPress={() => setFirstHalf(false)}
-        >
-          Julho - Dezembro
-        </Button>
-      </HStack>
+            <VictoryAxis
+              dependentAxis
+              animate
+              style={{
+                axis: { stroke: 'transparent' },
+                ticks: { stroke: 'transparent' },
+                tickLabels: {
+                  fill: 'transparent',
+                },
+                grid: {
+                  stroke: colorPallet.primary[50],
+                  opacity: '0.4',
+                },
+              }}
+            />
+            <VictoryBar
+              style={{
+                data: {
+                  fill: (d) => d.datum.fill,
+                },
+                labels: {
+                  fontSize: 12,
+                  fontWeight: 'bold',
+                  fill: (d) => d.datum.fill,
+                },
+              }}
+              data={analyticsChartData.map((value, index) =>
+                (currentMonth === index &&
+                  isFirstHalf &&
+                  currentYear === selectedYear) ||
+                (currentMonth === index + 6 &&
+                  !isFirstHalf &&
+                  currentYear === selectedYear)
+                  ? { ...value, fill: colorPallet.primary[100] }
+                  : { ...value, fill: theme.colors.muted[300] }
+              )}
+              x="month"
+              y="amount"
+              animate
+              barWidth={40}
+              cornerRadius={10}
+              labelComponent={<VictoryLabel />}
+              labels={analyticsChartData.map(({ amount }) =>
+                formatMoney(amount, 'Kzs')
+              )}
+            />
+          </VictoryChart>
+        </View>
 
-      <View
-        style={{
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: '#fff',
-        }}
-      >
-        <VictoryChart
-          width={Dimensions.get('screen').width}
-          theme={VictoryTheme.material}
-          animate
-        >
-          <VictoryAxis
-            animate
-            style={{
-              axis: { stroke: 'transparent' },
-              ticks: { stroke: 'transparent' },
-              tickLabels: {
-                fill: theme.colors.light[800],
-                fontSize: 16,
-                fontWeight: 'bold',
-              },
-              grid: {
-                stroke: 'transparent',
-              },
-            }}
-          />
-          <VictoryAxis
-            dependentAxis
-            animate
-            style={{
-              axis: { stroke: 'transparent' },
-              ticks: { stroke: 'transparent' },
-              tickLabels: {
-                fill: 'transparent',
-              },
-              grid: {
-                stroke: colorPallet.primary[50],
-                opacity: '0.4',
-              },
-            }}
-          />
-          <VictoryBar
-            style={{
-              data: {
-                fill: (d) => d.datum.fill,
-              },
-              labels: {
-                fontSize: 12,
-                fontWeight: 'bold',
-                fill: (d) => d.datum.fill,
-              },
-            }}
-            data={analyticsChartData.map((value, index) =>
-              (currentMonth === index &&
-                isFirstHalf &&
-                currentYear === selectedYear) ||
-              (currentMonth === index + 6 &&
-                !isFirstHalf &&
-                currentYear === selectedYear)
-                ? { ...value, fill: colorPallet.primary[100] }
-                : { ...value, fill: theme.colors.muted[300] }
-            )}
-            x="month"
-            y="amount"
-            animate
-            barWidth={40}
-            cornerRadius={10}
-            labelComponent={<VictoryLabel />}
-            labels={analyticsChartData.map(({ amount }) =>
-              formatMoney(amount, 'Kzs')
-            )}
-          />
-        </VictoryChart>
-      </View>
-
-      <HStack mt="9" space="5">
-        <Pressable
-          shadow="9"
-          onPress={() => navigate('AnalyticsIncome')}
-          android_ripple={androidRippleEffect}
-          bg="primary.50"
-          p="5"
-          borderRadius="2xl"
-          flex="1"
-        >
-          <Text color="white" fontSize="sm" fontFamily="body" mb="2">
-            Total de Ganhos
-          </Text>
-          <Text color="white" fontWeight="bold" fontSize="lg" fontFamily="body">
-            {formatMoney(188290, 'Kzs')}
-          </Text>
-        </Pressable>
-        <Pressable
-          shadow="9"
-          onPress={() => navigate('AnalyticsExpense')}
-          android_ripple={androidRippleEffect}
-          bg="light.800"
-          p="5"
-          borderRadius="2xl"
-          flex="1"
-        >
-          <Text color="white" fontSize="sm" fontFamily="body" mb="2">
-            Total de Gastos
-          </Text>
-          <Text color="white" fontWeight="bold" fontSize="lg" fontFamily="body">
-            {formatMoney(18290, 'Kzs')}
-          </Text>
-        </Pressable>
-      </HStack>
+        <HStack my="9" space="5">
+          <Pressable
+            shadow="9"
+            onPress={() => navigate('AnalyticsIncome')}
+            android_ripple={androidRippleEffect}
+            bg="primary.50"
+            p="5"
+            borderRadius="2xl"
+            flex="1"
+          >
+            <Text color="white" fontSize="sm" fontFamily="body" mb="2">
+              Total de Ganhos
+            </Text>
+            <Text
+              color="white"
+              fontWeight="bold"
+              fontSize="lg"
+              fontFamily="body"
+            >
+              {formatMoney(188290, 'Kzs')}
+            </Text>
+          </Pressable>
+          <Pressable
+            shadow="9"
+            onPress={() => navigate('AnalyticsExpense')}
+            android_ripple={androidRippleEffect}
+            bg="light.800"
+            p="5"
+            borderRadius="2xl"
+            flex="1"
+          >
+            <Text color="white" fontSize="sm" fontFamily="body" mb="2">
+              Total de Gastos
+            </Text>
+            <Text
+              color="white"
+              fontWeight="bold"
+              fontSize="lg"
+              fontFamily="body"
+            >
+              {formatMoney(18290, 'Kzs')}
+            </Text>
+          </Pressable>
+        </HStack>
+      </ScrollView>
     </Container>
   );
 };
