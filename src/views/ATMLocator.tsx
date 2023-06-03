8d900ee3bba38cc82da7547544b9a136522c928b
Angleu <angleuzua99@gmail.com>
2023-06-03 19:15:30 +0100

    create base screen

--- src/views/ATMLocator.tsx
+++ src/views/ATMLocator.tsx
@@ -1,10 +1,13 @@
+import { View } from 'react-native';
 import React from 'react';
-import { Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 const ATMLocator = () => {
   return (
     <View>
-      <Text>ATMLocator</Text>
+      <MapView style={{ width: '100%', height: '100%' }} 
+        
+      />
     </View>
   );
 };
