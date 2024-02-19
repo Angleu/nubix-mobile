42f461ab5058834bd8c8fd6ccbc440ae5b642724
Angleu Silva <angleu.silva@espaes.com>
2024-02-19 13:06:56 +0100

    backup

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,8 @@
 import _axios from 'axios';
 
 const axios = _axios.create({
-  baseURL: 'http://44.204.63.25:3031/api',
+  baseURL: 'http://localhost:3000/api',
+  // baseURL: 'http://44.204.63.25:3031/api',
 });
 
 export default axios;
